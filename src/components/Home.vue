24e13cf69a959fff5453deb20d3aaf4e9105ed3c
GuoChun <1770740287@qq.com>
2020-07-31 16:07:39 +0800

    完成了登录功能

---
+++ src/components/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>home组件</div>
+</template>
+
+<script>
+export default {
+
+}
+
+</script>
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import './plugins/element.js'
+import 'plugins/element.js'
+
+// 导入全局样式表
+import 'assets/css/global.css'
+// 引入字体图标
+import 'assets/fonts/iconfont.css'
+
+//引入配置网络模块
+import axios from 'axios'
+axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
