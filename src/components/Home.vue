133c187ab06a303520c61c81f5d2eeebcd383c46
GuoChun <1770740287@qq.com>
2020-07-31 17:01:16 +0800

    添加退出登录功能

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,19 @@
 <template>
-  <div>home组件</div>
+  <div>
+    <el-button type="info" @click="logout">退出</el-button>
+  </div>
 </template>
 
 <script>
 export default {
-
+  methods: {
+    logout() {
+      // 清除token
+      window.sessionStorage.clear()
+      // 重定向会登录页面
+      this.$router.push('/login')
+    }
+  }
 }
-
 </script>
-<style>
-</style>
+<style></style>
--- src/main.js
+++ src/main.js
@@ -8,7 +8,7 @@ import 'assets/css/global.css'
 // 引入字体图标
 import 'assets/fonts/iconfont.css'
 
-//引入配置网络模块
+// 引入配置网络模块
 import axios from 'axios'
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
 Vue.prototype.$http = axios
